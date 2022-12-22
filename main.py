6b872930f7eabc0da7aa56ba3bc4955a89ee20e5
fsoctic <118767373+fsoctic@users.noreply.github.com>
2022-12-22 17:15:36 +0100

    Added template and css

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def home():
+  return render_template("index.html", content="user")
 
-@app.route("/test")
-def test():
-  return "Woooooooah"
+@app.route("/<name>")
+def name(name):
+  return render_template("index.html", content=name)
 
-@app.route("/path")
-def path():
-  return "<h1>This is crazy shiiiit</h1>"
+@app.route("/admin/")
+def admin():
+  return redirect(url_for("name", name="Admin!"))
 
 if __name__ == "__main__":
   app.run()
